b4ace145285a9898e37c66b0d30d6fc4760aa159
Matias Basto <bastom1@udayton.edu>
2025-09-03 23:10:49 -0400

    Tested Set Up

---
+++ TestSetup.java
@@ -0,0 +1,8 @@
+public class TestSetup {
+    public static void main(String[] args) {
+        System.out.println("✅ VS Code is working with Java!");
+        System.out.println("Java version: " + System.getProperty("java.version"));
+        System.out.println("Operating System: " + System.getProperty("os.name"));
+    }
+    
+}
