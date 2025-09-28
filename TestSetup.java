9b46c8b4c5ec3b36725bffac009dd7283adaee88
Matias Basto <bastom1@udayton.edu>
2025-09-27 22:09:23 -0400

    Tried previous test questions

--- TestSetup.java
+++ TestSetup.java
@@ -2,12 +2,29 @@ import java.util.*;
 
 public class TestSetup {
     public static void main(String[] args) {
-       ArrayList<String> list = new ArrayList<>();
-       list.add("Hello, World!");
-       list.add("This is a test.");
-         for (String s : list) {
-              System.out.println(s);
-         }
+       Scanner scnr = new Scanner(System.in);
+       String prompt = "Enter a number: ";
+
+       int input = getIntegerInput(scnr, prompt);
+       System.out.println("You entered: " + input);
+
+
     }
-    
+
+    private static int getIntegerInput(Scanner scnr, String prompt) {
+          while(true) {
+               System.out.print(prompt);
+
+               String input = scnr.nextLine();
+               try{
+                    int result = Integer.valueOf(input.trim());
+
+                    return result;
+               } catch(NumberFormatException e) {
+                    System.out.println("Invalid Input. Please enter valid integer.");
+               }
+               
+          }
+    }
+     
 }
